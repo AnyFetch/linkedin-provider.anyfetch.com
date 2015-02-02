e60efb66c2c798de4f7df22628eff19a486a7e6f
Neamar <neamar@neamar.fr>
2015-02-02 16:08:20 +0100

    Add linting in default CI

--- test/retrieve.js
+++ test/retrieve.js
@@ -1,6 +1,6 @@
 'use strict';
 
 require('should');
-var config = require('../config/configuration.js');
+// var config = require('../config/configuration.js');
 
 // Check data are retrieved from PROVIDER
--- test/upload.js
+++ test/upload.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('should');
-var request = require('supertest');
-var AnyFetchProvider = require('anyfetch-provider');
+// var request = require('supertest');
+// var AnyFetchProvider = require('anyfetch-provider');
 
 // Check data are uploaded onto AnyFetch
